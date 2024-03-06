0f224b872dcd2c2dae90bab770fb2f90b99966fc
Aven Sy <avenssy@gmail.com>
2024-03-07 02:53:29 +0800

    feat: added project in projects page

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,3 +1,5 @@
+import dinoRun from "/dino_run_picture.JPG?url"
+
 const Projects = () => {
     return (
       <div className="project-main-div">
@@ -6,7 +8,10 @@ const Projects = () => {
         </div>
 
         <div className="projects-tiles">
-          <img />
+          <a className="dino-run-div"href="https://avenssy.github.io/Dino-Run/">
+            <img src={dinoRun} className="tile-image" id="dino-run-id"/>     
+            <label className="tile-label" for="dino-run-id">Dino Run Game</label> 
+          </a>
         </div>
       </div>
     )
